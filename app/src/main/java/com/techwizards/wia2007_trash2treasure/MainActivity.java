537d26829d45286365ec3c3fcbd25f22df3312cc
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-11-16 15:05:59 +0800

    Initial commit

---
+++ app/src/main/java/com/techwizards/wia2007_trash2treasure/MainActivity.java
@@ -0,0 +1,14 @@
+package com.techwizards.wia2007_trash2treasure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
