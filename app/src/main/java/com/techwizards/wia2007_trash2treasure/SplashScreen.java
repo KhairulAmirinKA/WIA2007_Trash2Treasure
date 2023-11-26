0b96c8fb3ba9786675d166dc3d605abf69368e41
kinderBono <nuraimandanialmohdzaki@gmail.com>
2023-11-27 05:12:45 +0800

    Added save logged in state

--- app/src/main/java/com/techwizards/wia2007_trash2treasure/SplashScreen.java
+++ app/src/main/java/com/techwizards/wia2007_trash2treasure/SplashScreen.java
@@ -3,6 +3,7 @@ package com.techwizards.wia2007_trash2treasure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -15,10 +16,23 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, Login.class);
-                startActivity(intent);
-                finish();
+                checkLogState();
             }
         }, 3000);
+    }
+
+    private void checkLogState() {
+        SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+
+        if (!isLoggedIn) {
+            Intent intent = new Intent(SplashScreen.this, Login.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
