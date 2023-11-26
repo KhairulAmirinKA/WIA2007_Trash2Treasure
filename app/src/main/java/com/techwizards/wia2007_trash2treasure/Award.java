72da7108f4d372f533007ee528017d3bb03dd323
kinderBono <nuraimandanialmohdzaki@gmail.com>
2023-11-26 19:15:57 +0800

    Created Award.java Fragment

---
+++ app/src/main/java/com/techwizards/wia2007_trash2treasure/Award.java
@@ -0,0 +1,30 @@
+package com.techwizards.wia2007_trash2treasure;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Award extends Fragment {
+    public Award() {}
+
+    public static Award newInstance() {
+        Award fragment = new Award();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_award, container, false);
+    }
+}
