7b2f2ee078cd7a0f7985c66636a484790c323415
kinderBono <nuraimandanialmohdzaki@gmail.com>
2023-11-26 21:39:35 +0800

    Updated Login and Register Page

---
+++ app/src/main/java/com/techwizards/wia2007_trash2treasure/Register.java
@@ -0,0 +1,30 @@
+package com.techwizards.wia2007_trash2treasure;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Register extends Fragment {
+    public Register() {}
+
+    public static Register newInstance() {
+        Register fragment = new Register();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
