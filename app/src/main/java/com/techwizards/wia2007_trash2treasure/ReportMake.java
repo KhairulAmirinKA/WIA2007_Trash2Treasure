c7f177d599fc5feb9c26c90c55c0685b8d14eb72
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2024-01-08 22:55:16 +0800

    New Report ui. tinggal Complaint Location.

--- app/src/main/java/com/techwizards/wia2007_trash2treasure/ReportMake.java
+++ app/src/main/java/com/techwizards/wia2007_trash2treasure/ReportMake.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ReportMake extends Fragment {
     public ReportMake() {
@@ -26,6 +28,31 @@ public class ReportMake extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_report_make, container, false);
+        View view=  inflater.inflate(R.layout.fragment_report_make, container, false);
+
+        //users can choose the local authority from drop down list
+        handleLocalAuthoritySpinner(view);
+
+        return view;
     }
+
+
+    //handle the name local authority in dropdown spinner
+    private void handleLocalAuthoritySpinner(View view) {
+        //get spinner
+        Spinner SpinnerReportLocalAuthority= view.findViewById(R.id.SpinnerReportLocalAuthority);
+
+        String[] options= {"Majlis Bandaraya Shah Alam", "Majlis Bandaraya Petaling Jaya", "Majlis Bandaraya Kuala Selangor"};
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_dropdown_item
+        , options);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        SpinnerReportLocalAuthority.setAdapter(adapter);
+    }
+
+
+
 }
